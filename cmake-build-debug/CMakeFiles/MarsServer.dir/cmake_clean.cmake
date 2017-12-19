95104d1940fe4abd6727a83ca9ba0099e96f5144
ChanJLee <1355425625@qq.com>
2017-12-19 23:00:00 +0800

    sync

---
+++ cmake-build-debug/CMakeFiles/MarsServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MarsServer.dir/main.cpp.o"
+  "MarsServer.pdb"
+  "MarsServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MarsServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/MarsServer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MarsServer.
+# This may be replaced when dependencies are built.
