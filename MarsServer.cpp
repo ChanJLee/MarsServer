3dc92995cd0097eb7a33479d00f0c4ea45727dc5
ChanJLee <1355425625@qq.com>
2018-01-01 20:05:27 +0800

    sync

--- MarsServer.cpp
+++ MarsServer.cpp
@@ -9,18 +9,21 @@
 #include <iostream>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <thread>
+#include <sstream>
+#include <stddef.h>
 
 void MarsServer::start()
 {
 	std::cout << "start server" << std::endl;
 
-	int socket_server = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_server < 0) {
+	mSocketServer = socket(AF_INET, SOCK_STREAM, 0);
+	if (mSocketServer < 0) {
 		std::cerr << "open socket failed" << std::endl;
 		return;
 	}
 
-	sig_t sig_result = signal(SIGTSTP, sig_pipe);
+	sig_t sig_result = signal(SIGTSTP, sigPipe);
 	if (sig_result == SIG_ERR) {
 		std::cerr << "signal error" << std::endl;
 		return;
@@ -29,18 +32,18 @@ void MarsServer::start()
 	std::cout << "signal success" << std::endl;
 
 	struct sockaddr_in server;
-	memset(&server, 0, sizeof(socket_server));
+	memset(&server, 0, sizeof(mSocketServer));
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
 	server.sin_port = htons(8765);
 
-	int bind_result = bind(socket_server, (const sockaddr *) &server, sizeof(sockaddr_in));
+	int bind_result = bind(mSocketServer, (const sockaddr *) &server, sizeof(sockaddr_in));
 	if (bind_result < 0) {
 		std::cerr << "bind error" << std::endl;
 		return;
 	}
 
-	int listen_result = listen(socket_server, 5);
+	int listen_result = listen(mSocketServer, 5);
 	if (listen_result < 0) {
 		std::cerr << "listen error" << std::endl;
 		return;
@@ -49,24 +52,22 @@ void MarsServer::start()
 	struct sockaddr_in client;
 	while (true) {
 		socklen_t client_len = sizeof(client);
-		int socket_client = accept(socket_server, (struct sockaddr *) &client, &client_len);
+		int socket_client = accept(mSocketServer, (struct sockaddr *) &client, &client_len);
 		if (socket_client < 0) {
 			std::cerr << "accept client failed" << std::endl;
 			continue;
 		}
 
-		pid_t pid = fork();
-		if (pid == 0) {
-			close(socket_server);
-			process_conn_server(socket_client);
+		if (mCurrentSocketClient >= 0) {
+			close(mCurrentSocketClient);
 		}
-		else {
-			close(socket_client);
-		}
+
+		std::thread thread(&MarsServer::processClient, this);
+		thread.join();
 	}
 }
 
-void MarsServer::sig_pipe(int sig)
+void MarsServer::sigPipe(int sig)
 {
 	std::cout << "accept sig" << std::endl;
 	if (sig == SIGTSTP) {
@@ -74,23 +75,48 @@ void MarsServer::sig_pipe(int sig)
 	}
 }
 
-void MarsServer::process_conn_server(int socket)
+void MarsServer::processClient()
 {
-	const int buffer_size = 1024;
-	long size = 0;
-	char buffer[buffer_size];  //定义数据缓冲区
-	for (;;) {
-		//等待读
-		for (size = 0; size == 0; size = read(socket, buffer, buffer_size));
-		//输出从客户端接收到的数据
-		printf("%s", buffer);
-
-		//结束处理
-		if (strcmp(buffer, "quit") == 0) {
-			close(socket);   //成功返回0，失败返回-1
-			return;
+	ptrdiff_t size = 0;
+	char buffer[BUFFER_SIZE];  //定义数据缓冲区
+	std::ostringstream os;
+	while (true) {
+		os.clear();
+		while ((size = read(mCurrentSocketClient, buffer, BUFFER_SIZE)) != -1) {
+			os.write(buffer, (std::streamsize) size);
 		}
-		sprintf(buffer, "%l bytes altogether\n", size);
-		write(socket, buffer, strlen(buffer) + 1);
+
+		const std::string &str = os.str();
+		processData(str.c_str(), str.size());
+
+		if (mPackage != NULL) {
+			write(mCurrentSocketClient, mPackage->getBuffer(), mPackage->getLen());
+			mPackage = NULL;
+		}
 	}
+}
+void MarsServer::processData(const char *buffer, std::streamsize size)
+{
+	std::cout << buffer << std::endl;
+}
+
+MarPackage::MarPackage(size_t mLen)
+	: mLen(mLen)
+{
+	mBuffer = new char[mLen];
+	memset(mBuffer, 0, mLen);
+}
+
+MarPackage::~MarPackage()
+{
+	delete mBuffer;
+}
+
+char *MarPackage::getBuffer() const
+{
+	return mBuffer;
+}
+int MarPackage::getLen() const
+{
+	return mLen;
 }
--- MarsServer.h
+++ MarsServer.h
@@ -5,13 +5,35 @@
 #ifndef MARSSERVER_MARSSERVER_H
 #define MARSSERVER_MARSSERVER_H
 
+#include <cstdio>
+#include <ios>
+
+typedef int SocketServer;
+
+const int BUFFER_SIZE = 1024;
+class MarPackage {
+private:
+	char* mBuffer;
+	int mLen;
+public:
+	MarPackage(size_t mLen);
+	~MarPackage();
+	char *getBuffer() const;
+	int getLen() const;
+};
+
 class MarsServer
 {
+private:
+	int mCurrentSocketClient = -1;
+	SocketServer mSocketServer;
+	MarPackage* mPackage;
 public:
 	void start();
 private:
-	void process_conn_server(int s);
-	static void sig_pipe(int signo);
+	void processClient();
+	void processData(const char* buffer, std::streamsize size);
+	static void sigPipe(int signo);
 };
 
 
